b27449f456da38f8b4bdae3755ac2880bb029aa8
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-30 22:31:14 +0200

    init

--- CustomSwitchInSwuftUI/ContentView.swift
+++ CustomSwitchInSwuftUI/ContentView.swift
@@ -9,13 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            self.show ? Color.black.edgesIgnoringSafeArea(.all) : Color.white.edgesIgnoringSafeArea(.all)
+            CustomSwitch(show: $show)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomSwitch: View {
+    @Binding var show: Bool
+    var body: some View {
+        ZStack {
+            Capsule()
+                .fill(Color.orange)
+                .frame(width: 135, height: 45)
+            HStack {
+                if show {
+                    Spacer()
+                    Text("Light")
+                        .foregroundColor(.gray)
+                }
+                ZStack {
+                    Capsule()
+                        .fill(Color.red)
+                        .frame(width: 60, height: 48)
+                    Image(systemName: "moon.fill")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .foregroundColor(.yellow)
+                }.onTapGesture {
+                    self.show.toggle()
+                }
+                if !show {
+                    Text("Dark")
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+            }.frame(width: 135, height: 45)
+                .animation(.spring())
+        }
     }
 }
